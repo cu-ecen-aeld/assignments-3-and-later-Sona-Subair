94deee269c0c8d2563ccc3cdf9b9c4d0a40287bd
Sona Subair <sosu5785@colorado.edu>
2022-09-04 15:13:06 -0600

    Modifications + Document validating the file type of cross compiler output

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <errno.h>
 
-
+//FUnction to explain the right usage of arguments
 void usage(){
     printf("\nExpected two additional arguments.");
     printf("\nArgument 1: Path to file.");
@@ -16,42 +16,50 @@ void usage(){
 }
 
 int main(int argc,char** argv){
-openlog("finder-log",LOG_PID|LOG_ERR,LOG_USER);  
-setlogmask(LOG_UPTO(LOG_DEBUG));
+
+// Checking if all the argument are provided by the user    
     if(argc !=3){
         usage();
         return(1);
     }
 
-    size_t count;
+//Configuring system log
+    openlog("finder-log",LOG_PID|LOG_ERR,LOG_USER);     
+    setlogmask(LOG_UPTO(LOG_DEBUG));
+    size_t string_length;
     ssize_t nr;
     char* file_path=argv[1];
     char* string=argv[2];
 
+//Opening file and checking if it was successfull
     int fd = open(file_path, O_RDWR| O_CREAT ,0644);
+
     if (fd == -1){
-    syslog (LOG_USER, "Unable to open file, Check the perssion");
-    syslog(LOG_ERR, "ERROR: %s", strerror(errno));
-    return(1);
+        syslog (LOG_USER, "Unable to open file, Check the perssion");
+        syslog(LOG_ERR, "ERROR: %s", strerror(errno));
+        return(1);
     }    
 
-count = strlen(string);
-syslog (LOG_DEBUG, "Wrinting %s to %s.",string,file_path);
+    string_length = strlen(string);
+    syslog (LOG_DEBUG, "Wrinting %s to %s.",string,file_path);
 
-nr = write(fd, string, count);
-if (nr == -1){
+//Writing to file and check if it was successfull
+    nr = write(fd, string, string_length);
+    if (nr == -1){
         syslog (LOG_USER, "Writing to file not Successfull");
         syslog(LOG_ERR, "Error: %s", strerror(errno));     
         return(1);
-}
-else if (nr != count){
+    }
+
+//Checking for partial write    
+    else if (nr != string_length){
         syslog (LOG_USER, "partial write");       
         return(1);
-}
-else
-syslog (LOG_USER, "Writing Successfull"); 
-
-close(fd);
-closelog();
-return(0);
+    }
+    else
+        syslog (LOG_USER, "Writing Successfull"); 
+ 
+    close(fd);
+    closelog();
+    return(0);
 }
