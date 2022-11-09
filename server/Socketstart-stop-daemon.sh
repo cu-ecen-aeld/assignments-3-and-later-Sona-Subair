48d98ec7b66b635dd7e25baa067a52443d879485
Sona Subair <sosu5785@colorado.edu>
2022-11-08 20:52:34 -0700

    init script

--- server/Socketstart-stop-daemon.sh
+++ server/Socketstart-stop-daemon.sh
@@ -6,10 +6,12 @@ case "$1" in
 	start)
 		echo "Starting daemon"
 		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+		/usr/bin/aesdchar_load aesdchar
 		;;
 	
 	stop)
 		echo "Stopping daemon"
+		/usr/bin/aesdchar_unload aesdchar
 		start-stop-daemon -K -n aesdsocket
 		;;
 	
